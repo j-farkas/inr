ea1b7797233d9e02aff8e320f74529d41846f7ab
Jared Farkas <jaredmfarkas@gmail.com>
2020-06-10 10:46:46 -0700

    initial creation

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace inr.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
